080672cf1e2220298782c97f6b694729732cc58f
YevhDr <evgesha.drozdova@gmail.com>
2017-09-13 21:42:54 +0300

    selectAll points

--- js/getStation.js
+++ js/getStation.js
@@ -1,8 +1,8 @@
  var getStation = function(stat) {
 
         if (stat === 'a') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(49.207883, 24.666714), 6);
             L.marker([49.207883, 24.666714], {icon: greenPoint}).bindTooltip("Бурштинська ТЕС", {
                 permanent: true,
@@ -13,9 +13,9 @@
         }
 
         if (stat === 'b') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
-             map.setView(new L.LatLng(48.463149, 38.202597), 6);
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            map.setView(new L.LatLng(48.463149, 38.202597), 6);
             L.marker([48.463149, 38.202597], {icon: greenPoint}).bindTooltip("Вуглегірська ТЕС", {
                 permanent: true,
                 direction: 'right'
@@ -25,8 +25,8 @@
         }
 
         if (stat === 'c') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(50.446638, 30.639666), 6);
             L.marker([50.446638, 30.639666], {icon: greenPoint}).bindTooltip("Дарницька ТЕС", {
                 permanent: true,
@@ -38,8 +38,8 @@
         }
 
         if(stat ==='e') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(47.5086, 34.6256), 6);
             L.marker([47.5086, 34.6256], {icon: greenPoint}).bindTooltip("Запорізька ТЕС", {
                 permanent: true,
@@ -50,8 +50,8 @@
         }
 
         if(stat === 'g') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(49.071129,24.319353), 6);
             L.marker([49.071129,24.319353], {icon: greenPoint}).bindTooltip("Калуська ТЕС", {
                 permanent: true,
@@ -62,20 +62,20 @@
         }
 
         if(stat ==='i') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(48.747775,37.573545), 6);
         L.marker([48.747775,37.573545], {icon: redPoint}).bindTooltip("Краматорська ТЕС", {
             permanent: true,
             direction: 'right'
         }).addTo(cities);
-            d3.select("p#temporary").remove();
+            d3.selectAll("p#temporary").remove();
             d3.select("div.text").append("p").attr("id","temporary").text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMak").attr("font-size", "0.6vh");
         }
 
         if(stat === 'j') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(47.566448,33.64328), 6);
         L.marker([47.566448,33.64328], {icon: greenPoint}).bindTooltip("Криворізька ТЕС", {
             permanent: true,
@@ -86,8 +86,8 @@
         }
 
         if(stat === 'k') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(47.99459,37.237741), 6);
         L.marker([47.99459,37.237741], {icon: greenPoint}).bindTooltip("Курахівська ТЕС", {
             permanent: true,
@@ -98,8 +98,8 @@
         }
 
         if(stat === 'l') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(48.706825,29.220188), 6);
         L.marker([48.706825,29.220188], {icon: greenPoint}).bindTooltip("Ладижинська ТЕС", {
             permanent: true,
@@ -110,8 +110,8 @@
         }
 
         if(stat ==='m') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(48.748741,39.260899), 6);
         L.marker([48.748741,39.260899], {icon: greenPoint}).bindTooltip("Луганська ТЕС", {
             permanent: true,
@@ -122,8 +122,8 @@
         }
 
         if(stat === 'n') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(48.403499,35.118088), 6);
         L.marker([48.403499,35.118088], {icon: redPoint}).bindTooltip("Придніпровська ТЕС", {
             permanent: true,
@@ -134,8 +134,8 @@
         }
 
         if(stat === 'o') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(48.871596,37.764766), 6);
         L.marker([48.871596,37.764766], {icon: greenPoint}).bindTooltip("Слов'янська ТЕС", {
             permanent: true,
@@ -146,8 +146,8 @@
         }
 
         if(stat === 'p') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(47.799096,38.004681), 6);
         L.marker([47.799096,38.004681], {icon: greenPoint}).bindTooltip("Старобешівська ТЕС", {
             permanent: true,
@@ -158,8 +158,8 @@
         }
 
         if(stat === 'q') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(49.797068,36.590277), 6);
         L.marker([49.797068,36.590277], {icon: greenPoint}).bindTooltip("ТЕЦ-2 (Есхар) ТЕС", {
             permanent: true,
@@ -170,8 +170,8 @@
         }
 
         if(stat === 'r') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(50.134847,30.745404), 6);
         L.marker([50.134847,30.745404], {icon: redPoint}).bindTooltip("Трипільська ТЕС", {
             permanent: true,
@@ -182,8 +182,8 @@
         }
 
         if(stat === 's') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(49.386221,32.060906), 6);
         L.marker([49.386221,32.060906], {icon: greenPoint}).bindTooltip("Черкаська ТЕС", {
             permanent: true,
@@ -194,8 +194,8 @@
         }
 
         if(stat === 't') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(51.455278,31.262222), 6);
         L.marker([51.455278,31.262222], {icon: greenPoint}).bindTooltip("Чернігівська ТЕС", {
             permanent: true,
@@ -206,8 +206,8 @@
         }
 
         if(stat === 'v' || stat === 'y') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(49.5865,36.52525), 6);
         L.marker([49.5865,36.52525], {icon: greenPoint}).bindTooltip("Зміївська ТЕС", {
             permanent: true,
@@ -224,8 +224,8 @@
         }
 
      if(stat === 'x'|| stat === 'f') {
-         d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-         d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+         d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+         d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
          map.setView(new L.LatLng(48.480556,38.289722), 6);
          L.marker([49.5865,36.52525], {icon: redPoint}).bindTooltip("Миронівська ТЕС", {
              permanent: true,
@@ -236,14 +236,13 @@
      }
 
         if(stat === 'z') {
-            d3.select("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
-            d3.select("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
+            d3.selectAll("img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").remove();
+            d3.selectAll("div.leaflet-tooltip.leaflet-zoom-animated.leaflet-tooltip-right").remove();
             map.setView(new L.LatLng(50.219662,24.373773), 6);
             L.marker([50.219662,24.373773], {icon: greenPoint}).bindTooltip("Добротвірська ТЕС", {
                 permanent: true,
                 direction: 'right'
             }).addTo(cities);
-            d3.select("p#temporary").remove();
             d3.select("div.text").append("p").attr("id","temporary").text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMak").attr("font-size", "0.6vh");
 
         }
--- js/leaf-demo.js
+++ js/leaf-demo.js
@@ -1,7 +1,58 @@
 
+var redPoint = L.icon({
+    iconUrl: 'img/red.svg',
+    iconSize:     [20, 20], // size of the icon
+
+    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+
+    popupAnchor:  [-10, -10] // point from which the popup should open relative to the iconAnchor
+});
+
+var greenPoint = L.icon({
+    iconUrl: 'img/green.svg',
+    iconSize:     [20, 20], // size of the icon
+
+    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+
+    popupAnchor:  [-10, -10] // point from which the popup should open relative to the iconAnchor
+});
 
 var cities = L.layerGroup();
 
+////////////////////////////////////////
+// add stations/////////////////////////
+///////////////////////////////////////
+
+
+L.marker([49.207883, 24.666714], {icon: greenPoint}).bindTooltip("Бурштинська ТЕС", {
+    permanent: true,
+    direction: 'right'
+}).addTo(cities);
+
+L.marker([48.463149, 38.202597], {icon: greenPoint}).bindTooltip("Вуглегірська ТЕС", {
+    permanent: true,
+    direction: 'right'
+}).addTo(cities);
+
+L.marker([50.446638, 30.639666], {icon: greenPoint}).bindTooltip("Дарницька ТЕС", {
+    permanent: true,
+    direction: 'right'
+}).addTo(cities);
+
+
+L.marker([47.5086, 34.6256], {icon: greenPoint}).bindTooltip("Запорізька ТЕС", {
+    permanent: true,
+    direction: 'right'
+}).addTo(cities);
+
+L.marker([49.071129,24.319353], {icon: greenPoint}).bindTooltip("Калуська ТЕС", {
+    permanent: true,
+    direction: 'right'
+}).addTo(cities);
+
+///////////////////////////////////
+////////////////////////////////
+///////////////////////////////////
 
 var mbAttr = "&copy;<a href='http://www.openstreetmap.org/copyright'>mapbox</a>";
 mbUrl = 'https://api.mapbox.com/styles/v1/evgeshadrozdova/cj7ive7fj5qfw2sr0sa76a748/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNqMjZuaGpkYTAwMXAzMm5zdGVvZ2c0OHYifQ.s8MMs2wW15ZyUfDhTS_cdQ';
@@ -22,43 +73,3 @@ var baseLayers = {
 var overlays = {
     "Cities": cities
 };
-
-
-var jUrl = "data/stations_ids.csv";
-
-
-
-var redPoint = L.icon({
-    iconUrl: 'img/red.svg',
-    iconSize:     [20, 20], // size of the icon
-
-    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
-
-    popupAnchor:  [-10, -10] // point from which the popup should open relative to the iconAnchor
-});
-
-var greenPoint = L.icon({
-    iconUrl: 'img/green.svg',
-    iconSize:     [20, 20], // size of the icon
-
-    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
-
-    popupAnchor:  [-10, -10] // point from which the popup should open relative to the iconAnchor
-});
-
-
-// d3.csv(jUrl, function(data) {
-//
-//     console.log(data);
-//
-//     for ( var i=0; i < data.length; ++i )
-//     {
-//         L.marker( [data[i].latitude, data[i].longitude], {icon: redPoint, opacity: 0.5})
-//             // .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + data[i].name + '</a>' )
-//             .addTo( map );
-//     }
-//
-//
-// });
-
-// $(window).on("resize", function () { $('#map').height($(window).height()); map.invalidateSize(); }).trigger("resize");
