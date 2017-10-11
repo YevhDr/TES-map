e822d74779a76f7b3fb0625e440c72b361facb08
YevhDr <evgesha.drozdova@gmail.com>
2017-10-11 19:38:47 +0300

    new code for table and sparklines

--- js/spark(coal_new).js
+++ js/spark(coal_new).js
@@ -2,7 +2,7 @@ var spark = function () {
 
     var width = 120;
     var height = 30;
-    var margin = {top: 10, bottom: 0, left:0, right:0};
+    var margin = {top: 0, bottom: 0, left:0, right:0};
 
     function chart(selection) {
 
