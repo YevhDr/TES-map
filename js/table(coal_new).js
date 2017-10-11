22ac0cf2ab317f194f01ceb21b6d903c583c5090
YevhDr <evgesha.drozdova@gmail.com>
2017-10-11 22:03:15 +0300

    add double sort and remov st.name

--- js/table(coal_new).js
+++ js/table(coal_new).js
@@ -1,12 +1,12 @@
 var file = "data/coal_new.json";
 
-d3.json(file, function(error, data) {
+d3.json(file, function (error, data) {
     if (error) throw error;
 
     d3.select("p.date")
         .append("text")
         .text("Останнє оновлення: " + data[0].date)
-        .style("color","white");
+        .style("color", "white");
 
     data.forEach(function (d) {
         d.completance = +d.completance;
@@ -38,51 +38,62 @@ d3.json(file, function(error, data) {
         .data(data)
         .enter()
         .append('tr')
-        .attr("id", function(d){ return d.id })
-        .attr("onclick", function(d){ return "getStation('" + d.id +"')" });
+        .attr("id", function (d) {
+            return d.id
+        })
+        .attr("onclick", function (d) {
+            return "getStation('" + d.id + "')"
+        });
 
-// Add the stock name cell.
+
     rows.append('td')
+        .attr("id", function (d) {
+            return d.id
+        })
 
         .text(function (d) {
             return d.station;
         });
 
-// Add the stock name cell.
+
     rows.append('td')
         .text(function (d) {
             return d.completance;
         });
 
 
-//Add the barcode chart.
+    //Add the spark chart.
     rows.append('td')
         .datum(function (d) {
             return d.mentions;
         })
         .call(spark());
 
-// Add the number of mentions by hour, aligned to the right.
-//     rows.append('td')
-//         .text(function (d) {
-//             return d.plan_percent;
-//         });
-
-
-
-
+    //double sort by numver and then by name
     table.selectAll("tbody tr")
-        .sort(function(a, b) {
-            return d3.ascending(a.plan_percent, b.plan_percent) ;
+        .sort(function (a, b) {
+            return d3.ascending(a.plan_percent, b.plan_percent) ||
+                d3.ascending(a.mentions[0].coal_type, b.mentions[0].coal_type)
         });
 
-   d3.select("tr#f").attr("class", "no-bottom-border");
-
-   d3.select("tr#y").attr("class", "no-bottom-border");
 
 
+    //remove value of selected
+    d3.select("td#y").text("");
+    d3.select('td#f').text("");
 
-});
+
+
+    // remove bottom-border of the rows
+    d3.select("tr#v")
+        .attr("class", "no-bottom-border");
+
+    d3.select("tr#x")
+        .attr("class", "no-bottom-border");
+
+
+})
+;
 
 
 
